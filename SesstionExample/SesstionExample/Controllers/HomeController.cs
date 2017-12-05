a2f1b4feb8502f95bd8da5090e63fd98756bd703
Alexander <alexander.hontariev@gmail.com>
2017-12-05 17:48:30 +0200

    Пример работы куки
    Реализация кук в HomeController action Index

--- SesstionExample/SesstionExample/Controllers/HomeController.cs
+++ SesstionExample/SesstionExample/Controllers/HomeController.cs
@@ -8,9 +8,14 @@ namespace SesstionExample.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
         public ActionResult Index()
         {
+            var cookie = new HttpCookie("test_cookie", DateTime.Now.ToString("dd.MM.yyyy"))
+            {
+                Expires = DateTime.Now.AddMinutes(10)
+            };
+            Response.SetCookie(cookie);
+
             return View();
         }
     }
